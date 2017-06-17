17372ed15ad81c89c3ab6c57313a7977492cdf12
Mohamed Elshenawy <mohamed211111@gmail.com>
2017-06-17 06:15:02 +0200

    Added tree height solution

--- Week1/tree_height/tree-height.py
+++ Week1/tree_height/tree-height.py
@@ -1,25 +1,30 @@
 # python3
 
-import sys, threading
-sys.setrecursionlimit(10**7) # max depth of recursion
+import sys
+import threading
+sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
+
 
 class TreeHeight:
         def read(self):
                 self.n = int(sys.stdin.readline())
                 self.parent = list(map(int, sys.stdin.readline().split()))
+                self.nodes = [0] * self.n
+        
+        def len(self, node):
+                parent = self.parent[node]
+                if parent == -1:
+                        return 1 
+                if self.nodes[node]:
+                        return self.nodes[node]
+                
+                self.nodes[node] = 1 + self.len(self.parent[node])
+                return self.nodes[node]
 
         def compute_height(self):
-                # Replace this code with a faster implementation
-                maxHeight = 0
-                for vertex in range(self.n):
-                        height = 0
-                        i = vertex
-                        while i != -1:
-                                height += 1
-                                i = self.parent[i]
-                        maxHeight = max(maxHeight, height);
-                return maxHeight;
+                return max([self.len(i) for i in range(self.n)])
+                
 
 def main():
   tree = TreeHeight()
